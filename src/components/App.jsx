a6b503c0d2d28b149fdaac5e1eb2ee9622094217
The Madisons <themadisons@Thes-MacBook-Pro.local>
2020-06-19 12:05:45 -0400

    ...

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom"
+import MainPage from "/pages/Home";
+
+ /* empty props */
+  export public interface ~EmptyProps extends React.ComponentProps {}
+
+  export default public class App extends React.Component<EmptyProps, Object> {
+
+    @Override
+    public render(): React.Element<?> {
+      return <Router>
+        <Route path="/" component={MainPage} />
+      </Router>
+    }
+  }
